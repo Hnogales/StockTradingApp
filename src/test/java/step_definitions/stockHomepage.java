8aee375250db3c26aebd6c3bd31eafe26bdeca54
Hnogales <hdn2388@gmail.com>
2021-06-18 11:27:10 -0400

    Created step_definition class for testing

---
+++ src/test/java/step_definitions/stockHomepage.java
@@ -0,0 +1,24 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class stockHomepage {
+	
+	//Harold
+	@Given("I am on the Stock Homepage")
+	public void i_am_on_the_stock_homepage() {
+	   
+	}
+
+	@Given("click on {string}")
+	public void click_on(String string) {
+	    
+	}
+
+	@Then("I should be able to add a trade")
+	public void i_should_be_able_to_add_a_trade() {
+	    
+	}
+
+}
---
+++ src/test/java/step_definitions/stockLogin.java
@@ -0,0 +1,30 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stockLogin {
+	
+	//Jeff
+	@Given("I am on the signin page")
+	public void i_am_on_the_signin_page() {
+	   
+	}
+
+	@When("I enter correct sigin credentials username {string} password {string}")
+	public void i_enter_correct_sigin_credentials_username_password(String string, String string2) {
+	    
+	}
+
+	@When("I click on signin button")
+	public void i_click_on_signin_button() {
+	    
+	}
+
+	@Then("I should be directed to the login page")
+	public void i_should_be_directed_to_the_login_page() {
+	    
+	}
+
+}
