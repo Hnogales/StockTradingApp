91c71a250693fdd3a017ddc5d252a2281c7368dc
tayya <tayya@LAPTOP-NJLDV22N.fios-router.home>
2021-06-26 17:00:36 -0400

    I have created rest of the sted definition

--- src/test/java/step_definitions/stockHomepage.java
+++ src/test/java/step_definitions/stockHomepage.java
@@ -1,5 +1,6 @@
 package step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
@@ -20,5 +21,23 @@ public class stockHomepage {
 	public void i_should_be_able_to_add_a_trade() {
 	    
 	}
+	
+	
+	//Tayyaba
+	
+		@Given("I am external user of Stock Trade market APP")
+		public void i_am_external_user_of_Stock_Trade_market_APP() {
+			
+		}
+		
+		@And("I login")
+		public void i_login() {
+			
+		}
+		
+		@Then("Then I should be directed to the homepage of the application") 
+		public void Then_I_should_be_directed_to_the_homepage_of_the_application() {
+			
+		}
 
 }
