bf8cfafb99bee13e7d5ac9b0bb63144cec391708
Hnogales <hdn2388@gmail.com>
2021-06-28 09:16:11 -0400

    Added elements

--- src/test/java/pages/stockLoginElements.java
+++ src/test/java/pages/stockLoginElements.java
@@ -1,7 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utilities.Driver;
+
 public class stockLoginElements {
 	
-	// This class will have all our login page elements
+	// This class contains all our login page elements
+	
+	public stockLoginElements() {						
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+	
+	@FindBy (xpath="//input[contains(@id, 'username')]")
+	public WebElement usernameInputField;
+	
+	@FindBy (xpath="//input[contains(@id, 'password')]")
+	public WebElement passwordInputField;
+	
+	@FindBy (xpath="//button[contains(text(), 'Sign in')]")
+	public WebElement signinBtn;
 
 }
