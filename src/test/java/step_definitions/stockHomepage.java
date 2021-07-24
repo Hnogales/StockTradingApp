05fff5c851a4cb3999506dcc00ca1a35bf4a10dc
thayat2021 <tayyabahaya3@gmail.com>
2021-07-24 17:14:41 -0400

    created home page feature and step def

--- src/test/java/step_definitions/stockHomepage.java
+++ src/test/java/step_definitions/stockHomepage.java
@@ -57,6 +57,30 @@ public class stockHomepage {
 
 	}
 	
+	//Homepage  tayyaba
+	
+		@Given("I am external user of Stock Trade market APP")
+		public void i_am_external_user_of_stock_trade_market_app() {
+		    Driver.getDriver().get(PropertiesReader.getProperty("stockURL"));
+		 
+		}
+		@Given("I login")
+		public void i_login() {
+			stocklogin.usernameInputField.sendKeys("Sofia");
+			stocklogin.passwordInputField.sendKeys("SuperSofia123!");
+			stocklogin.signinBtn.click();
+		   
+		    
+		}
+		@Then("I should be directed to the homepage of the application")
+		public void i_should_be_directed_to_the_homepage_of_the_application() {
+
+		BrowserUtils utils = new BrowserUtils(); // calling utils
+		stockHomepageElements stockhomepage = new stockHomepageElements(); // calling stockHomePage
+		stockLoginElements stocklogin = new stockLoginElements();
+		Driver driver = new Driver();
+		}
+	
 	// Einar
 	@When("I click on {string} button")
 	public void i_click_on_button(String string) throws InterruptedException {
