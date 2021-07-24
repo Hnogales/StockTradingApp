f054b7149282b170c3cfd5312a65472bcffcf76a
thayat2021 <tayyabahaya3@gmail.com>
2021-07-24 17:04:09 -0400

    added my home page work

--- src/test/java/step_definitions/stockHomepage.java
+++ src/test/java/step_definitions/stockHomepage.java
@@ -8,32 +8,32 @@ import utilities.PropertiesReader;
 
 public class stockHomepage {
 	
-	stockLoginElements stocklogin = new stockLoginElements();
+	
 	
 	//Homepage
 	
-	@Given("I am external user of Stock Trade market APP")
-	public void i_am_external_user_of_stock_trade_market_app() {
-	    Driver.getDriver().get(PropertiesReader.getProperty("stockURL"));
-	 
-	}
-	@Given("I login")
-	public void i_login() {
-		stocklogin.usernameInputField.sendKeys("Sofia");
-		stocklogin.passwordInputField.sendKeys("SuperSofia123!");
-		stocklogin.signinBtn.click();
-	   
-	    
-	}
-	@Then("I should be directed to the homepage of the application")
-	public void i_should_be_directed_to_the_homepage_of_the_application() {
-	    
-		
-	}
+		@Given("I am external user of Stock Trade market APP")
+		public void i_am_external_user_of_stock_trade_market_app() {
+		    Driver.getDriver().get(PropertiesReader.getProperty("stockURL"));
+		 
+		}
+		@Given("I login")
+		public void i_login() {
+			stocklogin.usernameInputField.sendKeys("Sofia");
+			stocklogin.passwordInputField.sendKeys("SuperSofia123!");
+			stocklogin.signinBtn.click();
+		   
+		    
+		}
+		@Then("I should be directed to the homepage of the application")
+		public void i_should_be_directed_to_the_homepage_of_the_application() {
 
+		BrowserUtils utils = new BrowserUtils(); // calling utils
+		stockHomepageElements stockhomepage = new stockHomepageElements(); // calling stockHomePage
+		stockLoginElements stocklogin = new stockLoginElements();
+		Driver driver = new Driver();
+		}
 
-
-	
 
 
 
